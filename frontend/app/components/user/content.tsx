00dd9703655417e2e4741f1787c329733df5aa3a
mTy8421 <koontyza007@gmail.com>
2025-04-29 15:27:29 +0700

    update chart bar demo (user)

--- frontend/app/components/user/content.tsx
+++ frontend/app/components/user/content.tsx
@@ -1,3 +1,4 @@
+import { BarChart } from "~/lib/user/barChart"
 import Table from "./table"
 
 type Props = {}
@@ -6,8 +7,30 @@ export function HomeUser({ }: Props) {
   return (
     <>
       <div>
-        <p className="text-9xl text-center">Home Page</p>
-        <Table />
+        <p className="text-9xl text-center">Dashboard</p>
+
+        <div className="grid grid-cols-2">
+
+          <div className="col-span-1">
+            <div className="rounded overflow-hidden shadow-lg">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 text-center">Test</div>
+                <BarChart />
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-1">
+            <div className="rounded overflow-hidden shadow-lg">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 text-center">Test</div>
+                <BarChart />
+              </div>
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </>
   )
@@ -19,6 +42,7 @@ export function WorkUser({ }: Props) {
     <>
       <div>
         <p className="text-9xl text-center">WorkUser</p>
+        <Table />
       </div>
     </>
   )
---
+++ frontend/app/lib/user/barChart.tsx
@@ -0,0 +1,62 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+// import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'test',
+    },
+  },
+  responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false,
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const datas = ['10', '20', '30', '40', '50', '60', '70'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      // data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: datas,
+      backgroundColor: 'rgb(255, 99, 132)',
+      stack: 'Stack 0',
+    },
+  ],
+};
+
+export function BarChart() {
+  return <Bar options={options} data={data} />;
+}
