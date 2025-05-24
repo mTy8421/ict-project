fedea74bbe42012f938387392f81a93183a2a6f6
mTy8421 <koontyza007@gmail.com>
2025-05-24 20:51:37 +0700

    reset file

--- backend/src/upload-test/dto/update-upload-test.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUploadTestDto } from './create-upload-test.dto';
-
-export class UpdateUploadTestDto extends PartialType(CreateUploadTestDto) {}
--- backend/src/upload-test/upload-test.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UploadTestService } from './upload-test.service';
-import { UploadTestController } from './upload-test.controller';
-
-@Module({
-  controllers: [UploadTestController],
-  providers: [UploadTestService],
-})
-export class UploadTestModule {}
