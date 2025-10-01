8a6bee493ba79965bd6aba6c51e2bedc8a914bd8
mTy8421 <koontyza007@gmail.com>
2025-10-01 14:18:28 +0700

    ### Delete File ###

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,7 @@ import UserHome from "./pages/user/home";
 import UserWork from "./pages/user/work";
 import UserWorkLoad from "./pages/user/workload";
 import UserHistory from "./pages/user/history";
-import DeanDashboard from "./pages/dean/dashboard";
-import DeanWorkload from "./pages/dean/workload";
-import DeanWorkloadNew from "./pages/dean/workload-new";
-import DeanUsers from "./pages/dean/users";
-import StrategyDashboard from "./pages/vice-dean/strategy/dashboard";
-import StrategyWorkload from "./pages/vice-dean/strategy/workload";
-import AcademicDashboard from "./pages/vice-dean/academic/dashboard";
-import AcademicWorkload from "./pages/vice-dean/academic/workload";
-import DisciplineDashboard from "./pages/vice-dean/discipline/dashboard";
-import StudentQualityDashboard from "./pages/vice-dean/student-quality/dashboard";
+
 import Head from "./pages/head/head";
 import HeadWork from "./pages/head/work";
 import HeadHistory from "./pages/head/history";
@@ -98,26 +89,6 @@ export default function App() {
         <Route
           path="/admin/work/detail/:id"
           element={<AdminWorkStatusDetail />}
-        />
-
-        <Route path="/dean" element={<DeanDashboard />} />
-        <Route path="/dean/workload" element={<DeanWorkload />} />
-        <Route path="/dean/workload/new" element={<DeanWorkloadNew />} />
-        <Route path="/dean/users" element={<DeanUsers />} />
-        <Route path="/vice-dean/strategy" element={<StrategyDashboard />} />
-        <Route
-          path="/vice-dean/strategy/workload"
-          element={<StrategyWorkload />}
-        />
-        <Route path="/vice-dean/academic" element={<AcademicDashboard />} />
-        <Route
-          path="/vice-dean/academic/workload"
-          element={<AcademicWorkload />}
-        />
-        <Route path="/vice-dean/discipline" element={<DisciplineDashboard />} />
-        <Route
-          path="/vice-dean/student-quality"
-          element={<StudentQualityDashboard />}
         />
       </Routes>
     </BrowserRouter>
